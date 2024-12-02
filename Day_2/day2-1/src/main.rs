b509b33e7e5a6f9c693137cb56cb88ef6385d523
jdengi <jelledewit.engi@gmail.com>
2024-12-02 22:59:23 +0100

    solution day2-1

--- Day_2/day2-1/src/main.rs
+++ Day_2/day2-1/src/main.rs
@@ -1,5 +1,25 @@
-use shared::add;
+use shared::{parse_input, safe_increase, safe_decrease};
+use std::env::current_dir;
+use std::fs::read_to_string;
+
+fn select_fn(first: i32, last: i32) -> fn(i32, i32) -> bool {
+    if first > last {safe_decrease}
+    else {safe_increase} 
+}
 
 fn main() {
-    println!("Hello, world!: {}", add(1, 6));
+    println!("Starting in directory: {}", current_dir().unwrap().display());
+    
+    let data = read_to_string("data/input1.txt").unwrap();
+    let data = parse_input(data.as_str());
+
+    let safe_count = data.iter().filter(
+        |report| match report.first() > report.last() {
+            true => report.windows(2).all(|x| safe_decrease(x[0], x[1])),
+            false => report.windows(2).all(|x| safe_increase(x[0], x[1]))
+        }
+    ).count();
+
+    println!("There are: {} safe reports", safe_count)
+    
 }
--- Day_2/shared/src/lib.rs
+++ Day_2/shared/src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+pub fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    input.lines().map(
+        |s| s.split_ascii_whitespace().map(
+            |t| t.parse::<i32>().unwrap()
+        ).collect()
+    ).collect()
+}
+
+pub fn safe_increase(a: i32, b: i32) -> bool {
+    match b - a {
+        1..=3 => true,
+        _ => false
+    }
+}
+
+pub fn safe_decrease(a: i32, b: i32) -> bool {
+    match a - b {
+        1..=3 => true,
+        _ => false
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::read_to_string;
+    use std::env::current_dir;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn check_current_dir() {
+        println!("This test module is runnin in: {}", current_dir().unwrap().display());
+    }
+
+    #[test]
+    fn test_tokenization() {
+        let data = read_to_string("../data/test1.txt").unwrap();
+        let test_data = vec![
+            vec![7, 6, 4 ,2, 1],
+            vec![1, 2, 7, 8, 9],
+            vec![9, 7, 6, 2, 1],
+            vec![1, 3, 2, 4, 5],
+            vec![8, 6, 4, 4, 1],
+            vec![1, 3, 6, 7, 9]
+        ];
+
+        let data = parse_input(data.as_str());
+
+        assert_eq!(data, test_data, "parsed data does not match");
+    }
+
+    #[test]
+    fn test_safe_increase() {
+        assert!(safe_increase(1, 2));   // Minumum increase
+        assert!(safe_increase(1, 4));   // Maximum increase
+        assert!(!safe_increase(1, 5));  // Increase too large
+        assert!(!safe_increase(3, 3));  // Increase too small
+        assert!(!safe_increase(5, 3));  // Not an increase
+    }
+
+    #[test]
+    fn test_safe_decrease() {
+        assert!(safe_decrease(2, 1));   // Minumum decrease
+        assert!(safe_decrease(4, 1));   // Maximum decrease
+        assert!(!safe_decrease(5, 1));  // Decrease too large
+        assert!(!safe_decrease(3, 3));  // Decrease too small
+        assert!(!safe_decrease(3, 5));  // Not an decrease
     }
 }
