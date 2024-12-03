9ed8ae64a78876ad3372a2eba7e6ffbb6198621d
jdengi <jelledewit.engi@gmail.com>
2024-12-03 19:40:09 +0100

    Brute force solution for day2-2

--- Day_2/day2-2/src/main.rs
+++ Day_2/day2-2/src/main.rs
@@ -2,81 +2,77 @@ use shared::{parse_input, safe_decrease, safe_increase};
 use std::env::current_dir;
 use std::fs::read_to_string;
 
-fn is_safe_(input: &Vec<i32>) -> bool {
-    let mut safe_increases = 0;
-    let mut safe_decreases = 0;
-    let mut unsafe_creases = 0;
-
-    for x in input.windows(2){
-        match x[1] - x[0] {
-            -3..=-1 => safe_decreases += 1,
-            1..=3 => safe_increases += 1,
-            _ => unsafe_creases += 1
-        }
-    }
-
-    println!("I{} D{} U{}", safe_increases, safe_decreases, unsafe_creases);
-    if safe_increases > safe_decreases {
-        (safe_decreases + unsafe_creases) <= 1
-    } else {
-        (safe_increases + unsafe_creases) <= 1
-    }
+fn is_safe(input: &Vec<i32>, pred: fn(i32, i32)->bool) -> bool {
+    input.windows(2).all(|x| pred(x[0], x[1]))
 }
 
-fn is_safe(input: &Vec<i32>) -> bool {
+fn brute_force(input: &Vec<i32>) -> bool {
+
+    for idx in 0..input.len() {
+        let mut input_clone = input.clone();
+        input_clone.remove(idx);
+
+        // Check which operation we need to use to check
+        let check_fn = match input_clone.last() > input_clone.first() {
+            true => safe_increase,
+            false => safe_decrease
+        };
+
+        if is_safe(&input_clone, check_fn) { return true }
+    }
+
+    false
+}
+
+// Check if the report is safe or if it would be safe if bruteforced
+fn check_safe(input: &Vec<i32>) -> bool {
     // Check which operation we need to use to check
     let check_fn = match input.last() > input.first() {
         true => safe_increase,
         false => safe_decrease
     };
+    
+    // Check if the report is safe right from the start
+    if is_safe(input, check_fn) { return true }
+    // Otherwise, check if it's safe if we bruteforce it
+    else { return brute_force(input)}
+}
 
-    // Prime the loop
-    let mut input_iter = input.iter();
-    let mut a = input_iter.next().unwrap();
-    let mut b = input_iter.next().unwrap();
 
-    // Set fault condition to initial check
-    // println!("First check is: {} and {}", a, b);
-    let mut has_fault = !check_fn(*a, *b);
-    a = b;
-    if has_fault {
-        b = input_iter.next().unwrap();
+#[cfg(test)]
+mod tests {
+    // allow use of functions in this script
+    use super::*;
+
+    #[test]
+    fn test_sharp_increase()
+    {
+        let test_case = vec![1, 2, 10, 11, 12];
+
+        assert!(!check_safe(&test_case));
     }
 
-    // println!("Starting with: {}", has_fault);
+    #[test]
+    fn test_second_position_error() {
+        let test_case = vec![53, 54, 54, 55, 56, 57];
 
-    for x in input_iter {
-        if has_fault {
-            if !check_fn(*a, *x){
-                return false;
-            } else {
-                a = b;
-                b = x;
-            }
-
-        } else {
-            if !check_fn(*b, *x) {
-                b = x;
-                has_fault = true;
-            } else {
-                a = b;
-                b = x;
-            }
-        }
+        assert!(check_safe(&test_case))
     }
-
-    println!("{:?} is safe", input);
-
-    true
+    
+    #[test]
+    fn test_first_position_error() {
+        let test_case = vec![54, 53, 54, 55, 56, 57];
+        assert!(check_safe(&test_case))
+    }
 }
 
 fn main() {
     println!("Starting in directory: {}", current_dir().unwrap().display());
     
-    let data = read_to_string("data/test1.txt").unwrap();
+    let data = read_to_string("data/input1.txt").unwrap();
     let data = parse_input(data.as_str());
 
-    let safe_count = data.iter().filter(|&report| is_safe(report)).count();
+    let safe_count = data.iter().filter(|&report| check_safe(report)).count();
 
     println!("There are: {} safe reports with the Problem Dampener engaged", safe_count);
 }
